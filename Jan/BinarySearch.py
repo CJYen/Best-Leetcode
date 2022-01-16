5a3be27bd3275c397a21ab930b307e49b7f92789
CJYen <C.J.Yen@icloud.com>
2022-01-16 08:49:16 +0800

    修整 week 2 內容

--- Jan/BinarySearch.py
+++ Jan/BinarySearch.py
@@ -1,18 +1,21 @@
-# 704. Binary Search
 # from traceback import print_tb
 
+
 ''' 
+704. Binary Search
 預想:
 假設中間值剛好是我們要的target, 那我們就回傳該index
 假設target比中間值還小，就往左找
 相反的，就往右找
 
+例:
 left    m  -->   right
 [-1, 0, 3, 5, 9, 12] target 9
-
 '''
 
+
 def sol():
+
     nums = [-1, 0, 3, 5, 9, 12]
     target = 9
 
@@ -20,6 +23,7 @@ def sol():
     left, right = 0, len(nums) - 1
     # 當末置位大於首置位的時候，取中間值
     while left <= right:
+
         pivot = left + (right - left) // 2
         # 當中間值等於target就回傳index
         if nums[pivot] == target:
@@ -33,19 +37,9 @@ def sol():
         else:
             print("larger than target, search the left")
             right = pivot - 1
+
     print("cannot found, return -1")
     return -1
 
-    # I tried
-    # x = 0
-    # for i in nums:
-    #     if i < target:
-    #         x += 1
-    #     elif i == target:
-    #         nums[x] = i
-    #         print("x",x)
-    #         return x
-    #     else:
-    #         print(-1)
 
 sol()
--- Jan/FirstBadVersion.py
+++ Jan/FirstBadVersion.py
@@ -1,4 +1,5 @@
 '''
+278. First Bad Version
 在n裡面，如果bad的值之後，都會是false
 想像你是產品經理，當你知道某個版本的build是錯的，
 在它之後的build都會是錯的，試想你可以怎麼找出它們
@@ -7,6 +8,7 @@ Input:
 n = 5, bad = 4
 output:
 4
+
 在4以後的都是錯的
 *isBadVersion() api 已經預先處理好了
 '''
@@ -21,27 +23,21 @@ def isBadVersion(i):
         return False
 
 
-
 '''
 Time: O(n)
 Space: O(1)
 從頭一個一個找，直到找到為止
 
 '''
+
 def sol():
+
     # 在這個range裡，當呼叫的isBadVersion api回傳True時，回傳給我
     for i in range(n):
         if (isBadVersion(i)==True):
             return i
     # 否則就直接回傳n
     return n
-
-    # print(123)
-    # if len(n) == bad:
-    #     print("Fund it!")
-    #     return True
-    # elif len(n) > bad:
-    #     return False
 
 
 '''
@@ -50,9 +46,9 @@ Space: O(1)
 
 如果在遇到壞版本的情況下，就再繼續往更早的版本查下去，
 直到查到不能再查為止
-
 '''
 def sol2():
+
     left = 1
     right = n
         
@@ -60,6 +56,7 @@ def sol2():
     while left < right:
         mid = int(left + ( right - left) / 2)
         print("mid", mid)
+
         if isBadVersion(mid) == True:
             right = mid
             print("Bad version! ", int(right))
@@ -67,14 +64,10 @@ def sol2():
         else:
             left = mid + 1
             print("Now it's good version ",int(left))
+
     # 查到不能再查的時候，就回傳 left
     print("Found ",int(left))
     return int(left)
 
 
-
-
-
-
 sol2()
-
